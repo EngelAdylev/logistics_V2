b25b79b78be696675ea16078b4ff24d503f8382a
EngelAdylev <engel.adylev@gmail.com>
2026-05-25 05:52:19 +0300

    fix: eliminate N+1 queries in WagonService.getForMap with batch station loading

--- db-layer/src/main/java/ru/alabuga/dislocation/repository/WagonRepository.java
+++ db-layer/src/main/java/ru/alabuga/dislocation/repository/WagonRepository.java
@@ -1,9 +1,11 @@
 package ru.alabuga.dislocation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.alabuga.dislocation.model.Wagon;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +14,7 @@ public interface WagonRepository
                 QuerydslPredicateExecutor<Wagon> {
 
     Optional<Wagon> findByWagonNumber(String wagonNumber);
+
+    @Query("SELECT w FROM Wagon w WHERE w.stationCode IS NOT NULL")
+    List<Wagon> findAllWithStation();
 }
